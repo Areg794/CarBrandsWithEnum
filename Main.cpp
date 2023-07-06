1d63869e7d6dfb7041dfa5c750a788d534174ecb
Areg <arzumanyanareg192@gmail.com>
2023-07-06 20:19:06 +0400

    first commit

---
+++ Main.cpp
@@ -0,0 +1,65 @@
+
+#include <iostream>
+#include <string>
+
+enum class MyEnum {
+    Fiat,
+    Mercedes,
+    BMW,
+    Audi,
+    Subaru,
+    Lexus,
+    Honda,
+    Mini,
+    Porsche,
+    Polestar,
+    Tesla,
+    Toyota,
+    RollsRoyce,
+    Lamborghini,
+    };
+
+    class EnumConverter {
+    public:
+       static std::string ConvertToString(MyEnum enumValue) {
+            switch (enumValue) {
+            case MyEnum::Lamborghini:
+                return "Lamborghini";
+            case MyEnum::RollsRoyce:
+                return "RollsRoyce";
+            case MyEnum::Toyota:
+                return "Third";
+            case MyEnum::Tesla:
+                return "Tesla";
+            case MyEnum::Polestar:
+                return "Polestar";
+            case MyEnum::Porsche:
+                return "Porsche";
+            case MyEnum::Mini: 
+                return "Mini";
+            case MyEnum::Honda:
+                return "Honda";
+            case MyEnum::Lexus:
+                return "Lexus";
+            case MyEnum::Subaru:
+                return "Subaru";
+            case MyEnum::Audi:
+                return "Audi";
+            case MyEnum::BMW:
+                return "BMW";
+            case MyEnum::Mercedes:
+                return "Mercedes";
+            }
+        }
+    };
+
+    int main() {
+        MyEnum enumValue = MyEnum::Mercedes;
+        EnumConverter converter;
+
+        std::string stringValue = converter.ConvertToString(enumValue);
+
+        std::cout << "Car Brand: " << stringValue << std::endl;
+
+        return 0;
+    }
